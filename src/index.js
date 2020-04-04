d7616e83164a649595ef597b29cc3f56cddf0b0f
fsciuti <fsciuti@gmail.com>
2020-04-04 17:45:25 +0200

    feat: add state object

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import FakeData from './fake-data';
 import Items from './view/items';
 
-const items = FakeData.getItems();
+const state = {
+  items: FakeData.getItems(),
+};
 
 const app = document.querySelector('#app');
 
@@ -9,7 +11,7 @@ const input = document.createElement('input');
 input.className = 'form-control';
 input.addEventListener('input', (evt) => {
   let ulCloned = app.querySelector('.list-group').cloneNode(false);
-  const filteredItems = items.filter((item) => {
+  const filteredItems = state.items.filter((item) => {
     return item.toLowerCase().indexOf(evt.target.value.toLowerCase()) >= 0;
   });
   ulCloned = Items.createItemList(ulCloned, filteredItems);
@@ -21,7 +23,7 @@ app.appendChild(document.createElement('hr'));
 
 let ul = document.createElement('ul');
 ul.className = 'list-group';
-ul = Items.createItemList(ul, items);
+ul = Items.createItemList(ul, state.items);
 app.appendChild(ul);
 
 const button = document.createElement('button');
@@ -29,7 +31,7 @@ button.textContent = 'Aggiungi un elemento';
 button.className = 'btn btn-primary';
 button.addEventListener('click', () => {
   const newItem = FakeData.getItem();
-  items.push(newItem);
+  state.items.push(newItem);
   app.querySelector('.list-group').appendChild(Items.createItem(newItem));
   input.dispatchEvent(new Event('input'));
 });
