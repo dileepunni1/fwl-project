7de7c0f80309034b63de9ac92f90622f858bca5b
fsciuti <fsciuti@gmail.com>
2020-04-04 17:36:45 +0200

    feat: add view folder and items module

--- src/index.js
+++ src/index.js
@@ -1,39 +1,27 @@
 import FakeData from './fake-data';
+import Items from './view/items';
 
 const items = FakeData.getItems();
-
-const addItem = (item) => {
-  const li = document.createElement('li');
-  li.textContent = item;
-  li.className = 'list-group-item';
-  return li;
-};
-
-const addItemsToUl = (ul, itemsToAppend) => {
-  itemsToAppend.forEach((item) => {
-    ul.appendChild(addItem(item));
-  });
-};
 
 const app = document.querySelector('#app');
 
 const input = document.createElement('input');
 input.className = 'form-control';
 input.addEventListener('input', (evt) => {
-  const ulCloned = app.querySelector('.list-group').cloneNode(false);
+  let ulCloned = app.querySelector('.list-group').cloneNode(false);
   const filteredItems = items.filter((item) => {
     return item.toLowerCase().indexOf(evt.target.value.toLowerCase()) >= 0;
   });
-  addItemsToUl(ulCloned, filteredItems);
+  ulCloned = Items.createItemList(ulCloned, filteredItems);
   app.querySelector('.list-group').replaceWith(ulCloned);
 });
 app.appendChild(input);
 
 app.appendChild(document.createElement('hr'));
 
-const ul = document.createElement('ul');
+let ul = document.createElement('ul');
 ul.className = 'list-group';
-addItemsToUl(ul, items);
+ul = Items.createItemList(ul, items);
 app.appendChild(ul);
 
 const button = document.createElement('button');
@@ -42,7 +30,7 @@ button.className = 'btn btn-primary';
 button.addEventListener('click', () => {
   const newItem = FakeData.getItem();
   items.push(newItem);
-  app.querySelector('.list-group').appendChild(addItem(newItem));
+  app.querySelector('.list-group').appendChild(Items.createItem(newItem));
   input.dispatchEvent(new Event('input'));
 });
 app.appendChild(document.createElement('hr'));
