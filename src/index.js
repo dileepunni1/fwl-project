1991f214e55e76f7d0975193f7472b4a8e7be5b7
fsciuti <fsciuti@gmail.com>
2020-04-04 18:51:47 +0200

    feat: move elements to html

--- src/index.js
+++ src/index.js
@@ -13,38 +13,17 @@ const render = newState => {
   app = cloneApp;
 };
 
-const input = document.createElement('input');
-input.className = 'form-control';
-input.addEventListener('input', (evt) => {
+document.querySelector('[data-component=item-filter]').addEventListener('input', (evt) => {
   const filteredItems = state.items.filter((item) => {
     return item.toLowerCase().indexOf(evt.target.value.toLowerCase()) >= 0;
   });
   render({ ...state, items: filteredItems });
 });
 
-const ul = document.createElement('ul');
-ul.className = 'list-group';
-ul.setAttribute('data-component', 'item-list');
-
-const p = document.createElement('p');
-p.className = 'alert alert-primary';
-p.setAttribute('data-component', 'item-counter');
-
-const button = document.createElement('button');
-button.textContent = 'Aggiungi un elemento';
-button.className = 'btn btn-primary';
-button.addEventListener('click', () => {
+document.querySelector('[data-component=item-add]').addEventListener('click', () => {
   state.items.push(FakeData.getItem());
   render({ ...state });
 });
-
-app.before(input);
-app.appendChild(document.createElement('hr'));
-app.appendChild(p);
-app.appendChild(document.createElement('hr'));
-app.appendChild(ul);
-app.appendChild(document.createElement('hr'));
-app.after(button);
 
 window.requestAnimationFrame(() => {
   render({ ...state });
