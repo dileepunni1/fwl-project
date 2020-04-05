82d43afe802071764e7f9d6cd4261fcc2f68e370
fsciuti <fsciuti@gmail.com>
2020-04-05 12:54:07 +0200

    feat: update items component with shadowdom

--- src/view/web_components/items.js
+++ src/view/web_components/items.js
@@ -9,9 +9,42 @@ export default class ItemList extends VWCComponent {
 
   constructor() {
     super();
+    const shadowRoot = this.attachShadow({ mode: 'open' });
     this.ul = document.createElement('ul');
     this.ul.className = 'item-list list-group';
-    this.appendChild(this.ul);
+
+    const style = `
+    <style>
+      .list-group {
+        display: -ms-flexbox;
+        display: flex;
+        -ms-flex-direction: column;
+        flex-direction: column;
+        padding-left: 0;
+        margin-bottom: 0;
+      }
+      .list-group-item:first-child {
+        border-top-left-radius: .25rem;
+        border-top-right-radius: .25rem;
+      }
+      .list-group-item:last-child {
+        margin-bottom: 0;
+        border-bottom-right-radius: .25rem;
+        border-bottom-left-radius: .25rem;
+      }
+      .list-group-item {
+        position: relative;
+        display: block;
+        padding: .75rem 1.25rem;
+        margin-bottom: -1px;
+        background-color: #fff;
+        border: 1px solid rgba(0,0,0,.125);
+      }
+    </style>
+      `;
+    shadowRoot.innerHTML = style;
+    shadowRoot.appendChild(document.createElement('slot'));
+    shadowRoot.appendChild(this.ul);
   }
 
   get items() {
